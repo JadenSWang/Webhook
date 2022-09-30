55fac1b1b78a51df6710e97049fa90b6b5de50c5
Jaden Wang <Jaden.Shiteng.Wang@gmail.com>
2022-09-30 15:35:56 -0700

    test

--- index.ts
+++ index.ts
@@ -15,3 +15,9 @@ app.post('/webhook-8cd2e298-4beb-4349-bfee-dc1749578e5b', (req, res) => {
 
   res.sendStatus(200)
 })
+
+app.post('/webhook-96ec5261-99ca-4061-9dd1-1de59e2bb228', (req, res) => {
+  exec('cd /home/dockerbuild/dgm-backend && npm run build:dev')
+
+  res.sendStatus(200)
+})
