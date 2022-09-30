7d3afcc30c772c2f82e6fd87996f48000b310cbb
Jaden Wang <Jaden.Shiteng.Wang@gmail.com>
2022-09-30 15:52:30 -0700

    bombers

--- index.ts
+++ index.ts
@@ -11,13 +11,13 @@ app.listen(port, () => {
 
 // webhook rebuild dg-backend-rapid
 app.post('/webhook-8cd2e298-4beb-4349-bfee-dc1749578e5b', (req, res) => {
-  exec('cd /home/dockerbuild/dgm-backend && npm run build:rapid')
+  exec('cd /home/dockerbuild/dgm-backend && npm run build:rapid-webhook')
 
   res.sendStatus(200)
 })
 
 app.post('/webhook-96ec5261-99ca-4061-9dd1-1de59e2bb228', (req, res) => {
-  exec('cd /home/dockerbuild/dgm-backend && npm run build:dev')
+  exec('cd /home/dockerbuild/dgm-backend && npm run build:dev-webhook')
 
   res.sendStatus(200)
 })
